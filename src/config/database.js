7eb9751380f6074d6e07976f5ea65d25e455a515
Dino Tudor <dinotudor@gmail.com>
2019-08-28 15:25:59 +0200

    folders structure, config sequelize, add pg and pg-hstore

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscpredAll: true,
+  },
+};
